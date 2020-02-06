1bca4a95dda935ac0a024de5305f0bd7e3508199
bacon-GIT <jbacon42@icloud.com>
2020-02-05 22:33:39 -0500

    Formatting, new title, new pages, far more todo. Need to create content management for blog and policy page

--- mainflask.py
+++ mainflask.py
@@ -1,8 +1,10 @@
-# My example webpage as well as usage syntax.
-# I recommend you build something with a bit more flair
+# FLASK_APP=mainflask.py
+# Developed by Josh Bacon
+# Support: bacon@uky.edu
+#
 
 from flask import Flask, render_template
-import xmlparse
+import uscertparse
 import datetime
 
 app = Flask(__name__)
@@ -12,16 +14,26 @@ app = Flask(__name__)
 def table():
     timedate = ("{}\t|\t{}".format(datetime.time(), datetime.date.today()))
     return render_template("homepage.html",
-                           vuln1=xmlparse.parseAlerts(0),
-                           vuln2=xmlparse.parseAlerts(1),
-                           vuln3=xmlparse.parseAlerts(2),
-                           vuln4=xmlparse.parseAlerts(3),
-                           vuln5=xmlparse.parseAlerts(4),
-                           vuln6=xmlparse.parseAlerts(5),
+                           vuln1=uscertparse.parseAlerts(0),
+                           vuln2=uscertparse.parseAlerts(1),
+                           vuln3=uscertparse.parseAlerts(2),
+                           vuln4=uscertparse.parseAlerts(3),
+                           vuln5=uscertparse.parseAlerts(4),
+                           vuln6=uscertparse.parseAlerts(5),
                            timeAndDate=timedate,
                            header_img="/static/title.png")  # Expandable, just change the parameter
 
 
-@app.route('/moredetails/')
-def moredetails():
-    return render_template("moredetails.html", moredetails=xmlparse.parseAlerts(1))
+@app.route('/KaliDocs/')
+def kalidocs():
+    return render_template("kalidocs.html")
+
+
+@app.route('/wereworkingonthat/')
+def wereworkingonthat():
+    return render_template("wereworkingonthat.html")
+
+
+@app.route('/policy/')
+def policy():
+    return render_template("policy.html")
